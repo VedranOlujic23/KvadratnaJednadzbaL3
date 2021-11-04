fb1205ab746c33dc601af5e63db5288632e3d824
Vedran Olujic <VOlujic@dspace.hr>
2021-11-04 18:39:19 +0100

    All files merged

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.CSharp.KvadratnaJednadzba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuadraticEquation qe = new QuadraticEquation(1, 2, -3);
+
+            var roots = qe.Roots;
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+            qe = new QuadraticEquation(-1, 2, -1);
+
+            roots = qe.Roots;
+            Console.WriteLine(roots[0]);
+            Console.WriteLine(roots[1]);
+
+            Console.ReadKey(false);
+            //DZ: ZADATAK 3 
+        }
+    }
+}
